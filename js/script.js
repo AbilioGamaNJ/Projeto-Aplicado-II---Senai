19736d1d2504952b974784bc5fbe52623868033b
Abilio <abiliogamanetojunior@gmail.com>
2024-06-30 00:30:23 -0300

    mudanças em geral

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ hamBurger.addEventListener("click", function () {
 });
 
 
+button_dados = document.querySelector(".buttonDados");
+
+button_dados.onclick = function() {
+    this.innerHTML = "<div class='loader'></div>";
+    setTimeout(() => {
+      this.innerHTML = "Dowload Concluído"
+      this.style = "background: #f1f5f4; color: #333;pointer-events: none";
+    }, 2000);
+}
